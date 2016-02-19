4b74d8a914426028192a54758e2a5b0cf93a6e99
Juan Sebastian <juansmacias@gmail.com>
2016-02-19 11:42:20 -0500

    Initial Commit

---
+++ BizagiTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BizagiTest
+//
+//  Created by Juan on 19/02/16.
+//  Copyright © 2016 juansmacias. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
